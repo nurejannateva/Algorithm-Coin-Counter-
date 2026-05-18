07adf8bac78260321c284fbc28d7c01214069e08
nurejannateva <nurejannateva39@gmail.com>
2026-05-18 08:38:06 +0600

    first commit

---
+++ coin.js
@@ -0,0 +1,25 @@
+function coinChanger(N) {
+  const coins = [500, 100, 50, 20, 10, 5, 2, 1];
+  let used = [];
+  let count = 0;
+
+  for (let coin of coins) {
+    // if coin is less than n
+    while (coin <= N) {
+      used.push(coin);
+      N -= coin;
+      count++;
+    }
+  }
+
+  console.log("Coins needed:", count);
+  console.log(used.join(","));
+}
+
+
+
+let numbers = [125, 48, 3];
+
+for (let i = 0; i < numbers.length; i++) {
+  coinChanger(numbers[i]);
+}
